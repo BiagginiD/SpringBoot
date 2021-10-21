1010d1b277568d16a691f7e0ac54cf342339ec85
Biaggini Durán Ribera <biaggini.duran@inetum.world>
2021-10-21 14:36:47 +0200

    Desarrollo practica 4

---
+++ src/main/java/com/gabrielCode/SpringBoot/App.java
@@ -0,0 +1,18 @@
+package com.gabrielCode.SpringBoot;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gabrielCode.beans.Mundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/gabrielCode/xml/beans.xml");
+		Mundo m = (Mundo) appContext.getBean("mundo");
+
+	}
+}
